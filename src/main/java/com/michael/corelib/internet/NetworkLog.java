0981919d059aa9d7b3016689594b1db41ca5c3ce
guoqing.sun <michael.s.china@gmail.com>
2015-07-17 17:00:38 +0800

    删除旧的DebugLog

--- src/main/java/com/michael/corelib/internet/NetworkLog.java
+++ src/main/java/com/michael/corelib/internet/NetworkLog.java
@@ -1,7 +1,6 @@
 package com.michael.corelib.internet;
 
 import com.michael.corelib.config.CoreConfig;
-import com.michael.corelib.log.DebugLog;
 
 /**
  * Created by michael on 15/1/6.
@@ -13,7 +12,7 @@ public class NetworkLog {
     public static final int SIG_PARAM_MAX_LENGTH = 5000;
 
     public static void LOGD(String msg) {
-        DebugLog.d("", msg);
+        CoreConfig.LOGD(msg);
     }
 
 }
